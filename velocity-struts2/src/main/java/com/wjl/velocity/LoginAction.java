d4dcf1a43f64c5b2ddfb792d6c5783279f99bfda
260601068 <260601068@qq.com>
2017-07-28 18:35:54 +0800

    add velocity-struts2

---
+++ velocity-struts2/src/main/java/com/wjl/velocity/LoginAction.java
@@ -0,0 +1,37 @@
+package com.wjl.velocity;
+
+import org.apache.struts2.ServletActionContext;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+
+@Controller
+@Scope("prototype")
+public class LoginAction extends ActionSupport{
+	private String username;
+	private String password;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String execute() throws Exception {
+		ActionContext.getContext().getSession().put("username", "中国");
+		System.out.println("pass: "+ServletActionContext.getRequest().getParameter("pass"));
+		return SUCCESS;
+	}
+}
---
+++ velocity-struts2/src/main/java/com/wjl/velocity/MyCustomerTool.java
@@ -0,0 +1,7 @@
+package com.wjl.velocity;
+
+public class MyCustomerTool {
+	public String addStr(String a,String b){
+		return a+b;
+	}
+}
